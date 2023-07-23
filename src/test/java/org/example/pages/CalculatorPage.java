155912ac71533407a3e86638cd100aca87b28bcc
Coskun Saglik <csaglik49@gmail.com>
2023-07-23 17:45:45 +0300

    Initial commit

---
+++ src/test/java/org/example/pages/CalculatorPage.java
@@ -0,0 +1,36 @@
+package org.example.pages;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.example.utils.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
+public class CalculatorPage {
+
+    public CalculatorPage() {
+        PageFactory.initElements(new AppiumFieldDecorator(Driver.getDriver()), this);
+    }
+
+    @AndroidFindBy(accessibility = "multiply")
+    public MobileElement multiply;
+
+    @AndroidFindBy(accessibility = "equals")
+    public MobileElement equals;
+
+    @AndroidFindBy(id = "com.google.android.calculator:id/result_final")
+    public MobileElement result;
+
+    /*
+    public void multiply() {
+        multiply.click();
+    } */
+
+    public void clickSingleDigit(int digit) {
+        String id = "com.google.android.calculator:id/digit_" + digit;
+        MobileElement number = Driver.getDriver().findElement(By.id(id));
+        number.click();
+    }
+
+
+}
